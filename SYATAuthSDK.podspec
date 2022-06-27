e212688adbc0c84869c22ba93bcdcce46097b800
雷子 <jianlei.chen@lucahealthcare.com>
2022-06-27 16:47:14 +0800

    2.12.6

--- SYATAuthSDK.podspec
+++ SYATAuthSDK.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'SYATAuthSDK'
-  s.version          = '0.0.1'
+  s.version          = '2.12.6'
   s.summary          = 'A short description of SYATAuthSDK.'
   s.description      = <<-DESC
 TODO: Add long description of the pod here.
--- SYATAuthSDK/Vendors/ATAuthSDK.framework/Headers/TXCustomModel.h
+++ SYATAuthSDK/Vendors/ATAuthSDK.framework/Headers/TXCustomModel.h
@@ -37,7 +37,7 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 #pragma mark- 全屏、弹窗模式设置
 /**
  *  授权页面中，渲染并显示所有控件的view，称content view，不实现该block默认为全屏模式
- *  实现弹窗的方案 x > 0 || y > 0 width < 屏幕宽度 || height < 屏幕高度
+ *  实现弹窗的方案 x >= 0 || y >= 0 width <= 屏幕宽度 || height <= 屏幕高度
  */
 @property (nonatomic, copy) PNSBuildFrameBlock contentViewFrameBlock;
 
@@ -50,6 +50,10 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, strong) UIColor *alertBlurViewColor;
 /** 底部蒙层背景透明度，默认0.5 */
 @property (nonatomic, assign) CGFloat alertBlurViewAlpha;
+/** contentView背景颜色，默认白色 */
+@property (nonatomic, strong) UIColor *alertContentViewColor;
+/** contentView背景透明度，默认1.0 ，即不透明*/
+@property (nonatomic, assign) CGFloat alertContentViewAlpha;
 /** contentView的四个圆角值，顺序为左上，左下，右下，右上，需要填充4个值，不足4个值则无效，如果值<=0则为直角 */
 @property (nonatomic, copy) NSArray<NSNumber *> *alertCornerRadiusArray;
 /** 标题栏背景颜色 */
@@ -71,8 +75,10 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, copy) PNSBuildFrameBlock alertCloseItemFrameBlock;
 
 #pragma mark- 导航栏（只对全屏模式有效）
-/**导航栏是否隐藏*/
+/**授权页显示中，导航栏是否隐藏，默认NO*/
 @property (nonatomic, assign) BOOL navIsHidden;
+/**授权页push到其他页面后，导航栏是否隐藏，默认NO*/
+@property (nonatomic, assign) BOOL navIsHiddenAfterLoginVCDisappear;
 /** 导航栏主题色 */
 @property (nonatomic, strong) UIColor *navColor;
 /** 导航栏标题，内容、字体、大小、颜色 */
@@ -93,18 +99,35 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 
 #pragma mark- 全屏、弹窗模式共同属性
 
-#pragma mark- 授权页弹出方向
-/** 授权页弹出方向，默认PNSPresentationDirectionBottom */
+#pragma mark- 授权页动画相关
+/** 授权页弹出方向，默认PNSPresentationDirectionBottom，该属性只对自带动画起效，不影响自定义动画 */
 @property (nonatomic, assign) PNSPresentationDirection presentDirection;
-
-/** 授权页显示和消失动画时间，默认为0.25s，<= 0 时关闭动画 **/
+/** 授权页显示和消失动画时间，默认为0.25s，<= 0 时关闭动画，该属性只对自带动画起效，不影响自定义动画 **/
 @property (nonatomic, assign) CGFloat animationDuration;
+
+/** 授权页显示动画（弹窗 & 全屏），不设置或设置为nil默认使用自带动画，SDK内部会主动更改动画的一些属性（包括：removedOnCompletion = NO、fillMode = kCAFillModeForwards 及 delegate） **/
+@property (nonatomic, strong, nullable) CAAnimation *entryAnimation;
+/** 授权页消失动画（弹窗 & 全屏），不设置或设置为nil默认使用自带动画，SDK内部会主动更改动画的一些属性（包括：removedOnCompletion = NO、fillMode = kCAFillModeForwards 及 delegate） **/
+@property (nonatomic, strong, nullable) CAAnimation *exitAnimation;
+
+/** 授权页显示时的背景动画（仅弹窗），不设置或设置为nil默认使用自带动画，SDK内部会主动更改动画的一些属性（包括：removedOnCompletion = NO、fillMode = kCAFillModeForwards 及 delegate） **/
+@property (nonatomic, strong, nullable) CAAnimation *bgEntryAnimation;
+/** 授权页消失时的背景动画（仅弹窗），不设置或设置为nil默认使用自带动画，SDK内部会主动更改动画的一些属性（包括：removedOnCompletion = NO、fillMode = kCAFillModeForwards 及 delegate） **/
+@property (nonatomic, strong, nullable) CAAnimation *bgExitAnimation;
 
 #pragma mark- 状态栏
 /** 状态栏是否隐藏，默认NO */
 @property (nonatomic, assign) BOOL prefersStatusBarHidden;
 /** 状态栏主题风格，默认UIStatusBarStyleDefault */
 @property (nonatomic, assign) UIStatusBarStyle preferredStatusBarStyle;
+
+#pragma mark- 背景
+/** 授权页背景色 */
+@property (nonatomic, strong) UIColor *backgroundColor;
+/** 授权页背景图片 */
+@property (nonatomic, strong) UIImage *backgroundImage;
+/** 授权页背景图片view的 content mode，默认为 UIViewContentModeScaleAspectFill */
+@property (nonatomic, assign) UIViewContentMode backgroundImageContentMode;
 
 #pragma mark- logo图片
 /** logo图片设置 */
@@ -197,6 +220,8 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, copy) NSArray<NSString *> *privacyTwo;
 /** 协议3，[协议名称,协议Url]，注：三个协议名称不能相同 */
 @property (nonatomic, copy) NSArray<NSString *> *privacyThree;
+/** 协议名称之间连接字符串数组，默认 ["和","、","、"] ，即第一个为"和"，其他为"、"，按顺序读取，为空则取默认 */
+@property (nonatomic, copy) NSArray<NSString *> *privacyConectTexts;
 /** 协议内容颜色数组，[非点击文案颜色，点击文案颜色] */
 @property (nonatomic, copy) NSArray<UIColor *> *privacyColors;
 /** 协议文案支持居中、居左设置，默认居左 */
@@ -209,6 +234,8 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, copy) NSString *privacyOperatorPreText;
 /** 运营商协议名称后缀文案，仅支持 >)]》）】』*/
 @property (nonatomic, copy) NSString *privacyOperatorSufText;
+/** 运营商协议指定显示顺序，默认0，即第1个协议显示，最大值可为3，即第4个协议显示*/
+@property (nonatomic, assign) NSInteger privacyOperatorIndex;
 /** 协议整体文案字体大小，小于12.0不生效 */
 @property (nonatomic, strong) UIFont *privacyFont;
 
@@ -218,6 +245,10 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
  *  最终会根据设置进来的width对协议文本进行自适应，得到的size是协议控件的最终大小
  */
 @property (nonatomic, copy) PNSBuildFrameBlock privacyFrameBlock;
+/**
+ *  未同意协议时点击登录按钮，协议整体文案的动画效果，不设置或设置为nil默认没有动画，SDK内部会主动更改动画的一些属性（包括：removedOnCompletion = NO、fillMode = kCAFillModeRemoved 及 delegate）
+ */
+@property (nonatomic, strong, nullable) CAAnimation *privacyAnimation;
 /** 协议整体相对屏幕底部的Y轴距离，与其他有区别！！不能小于0 */
 @property (nonatomic, assign) CGFloat privacyBottomOffetY DEPRECATED_MSG_ATTRIBUTE("Please use privacyFrameBlock instead");
 /** 协议整体（包括checkBox）相对content view的左右边距，当协议整体宽度小于（content view宽度-2*左右边距）且居中模式，则左右边距设置无效，不能小于0 */
@@ -235,6 +266,8 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, assign) CGFloat changeBtnTopOffetY DEPRECATED_MSG_ATTRIBUTE("Please use changeBtnFrameBlock instead");
 
 #pragma mark- 协议详情页
+/** 协议详情页容器是否自定义，默认NO，若为YES，则根据 PNSCodeLoginControllerClickProtocol 返回码获取协议点击详情信息 */
+@property (nonatomic, assign) BOOL privacyVCIsCustomized;
 /** 导航栏背景颜色设置 */
 @property (nonatomic, strong) UIColor *privacyNavColor;
 /** 导航栏标题字体、大小 */
